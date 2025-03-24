557c39c6c843855624cdf1781f7cf034815e009c
unknown <alainlado@gmail.com>
2025-03-24 21:50:10 +0800

    HomePage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,24 @@
+import { Container, VStack, Text, SimpleGrid } from "@chakra-ui/react"
+
 const HomePage = () => {
   return (
-    <div>Homepage</div>
+    <Container maxW="container.xl" py={12}>
+      <VStack spacing={8}>
+        <Text
+          fontSize={{ base: 22, sm: 28 }}
+          fontWeight={"bold"}
+          textTransform={"uppercase"}
+          textAlign={"center"}
+          bgGradient="to-r" gradientFrom="cyan.400" gradientTo="blue.500"
+          bgClip={"text"}
+        >
+          Current Product 🚀
+        </Text>
+        <SimpleGrid column={3} gap={2}>
+
+        </SimpleGrid>
+      </VStack>
+    </Container>
   )
 }
 
