902266e19a2e9e560fb07a22a8910835c013f8eb
unknown <alainlado@gmail.com>
2025-04-01 11:08:48 +0800

    rm env

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,29 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import { connectDB } from './config/db.js';
-import productRoutes from './routes/product.route.js';
+import express from "express";
+import dotenv from "dotenv";
+import productRoutes from "./routes/product.route.js";
+import path from "path";
+
+import { connectDB } from "./config/db.js";
 
 dotenv.config();
 
 const app = express();
-
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve(); // go to base path/route
 
 app.use(express.json()); // middle ware = allow us to Accept JSON data in the req.body
 
 app.use("/api/products", productRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
 app.listen(PORT, () => {
-    connectDB();
-    console.log('Server started at http://localhost:' + PORT);
-})
+  connectDB();
+  console.log("Server started at http://localhost:" + PORT);
+});
