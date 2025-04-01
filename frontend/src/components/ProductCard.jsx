e14816413c51246071f78fa0dadeb66022661bfa
unknown <alainlado@gmail.com>
2025-04-01 11:50:45 +0800

    Project Completed

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -89,7 +89,7 @@ const ProductCard = ({ product }) => {
       transition={"all 0.3s"}
       bg={bgColor}
       _hover={{ transform: "translate(0, -5px)", shadow: "xl" }}
-      minWidth={"30rem"}
+      minWidth={"20rem"}
     >
       <Toaster />
       <Image
